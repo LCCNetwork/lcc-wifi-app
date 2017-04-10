b361b33d199eba8006ef332e438905458b9c87a4
BuildTools <unconfigured@null.spigotmc.org>
2017-04-09 20:52:28 -0400

    Added support for iOS

---
+++ ios/Pods/Target Support Files/Pods-LCCWiFiApp-tvOS/Pods-LCCWiFiApp-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LCCWiFiApp_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_LCCWiFiApp_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-LCCWiFiApp-tvOSTests/Pods-LCCWiFiApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LCCWiFiApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_LCCWiFiApp_tvOSTests
+@end
